6e813a2842381d8d2167ea1841aafe03c619e9c8
Marcelo Brito <marcelobritowd@hotmail.com>
2016-08-12 23:59:59 -0300

    Ajeitando umas coisas

--- Strings/_1238.py
+++ Strings/_1238.py
@@ -1,28 +1,31 @@
-foo = input().split();
+vezes = int(input());
+contVezes = 1;
 
-fooParte1 = str(foo[0]);
-fooParte2 = str(foo[1]);
-
-StringFinal = ""
+while contVezes <= vezes:
+	foo = input().split();
+	fooParte1 = str(foo[0]);
+	fooParte2 = str(foo[1]);
+	StringFinal = ""
+			
+	cont = 0
+	while cont < len(list(fooParte1)) and cont < len(list(fooParte2)):
 		
-		
-cont = 0
-while cont <= (len(list(fooParte1)) and cont <= len(list(fooParte2))):
-	StringFinal += fooParte1[cont]
-	StringFinal += fooParte2[cont]
-	cont += 1
-
-i = 0;
-while i <= int(cont/2):
-	fooParte1Final = fooParte1.replace(fooParte1[i], "");
-	fooParte2Final = fooParte2.replace(fooParte2[i], "");
-	i += 1;
+		if (fooParte1[cont]):
+			StringFinal += fooParte1[cont]
+			
+		if (fooParte2[cont]):
+			StringFinal += fooParte2[cont]
+			
+		cont += 1
 	
-print(fooParte1Final)
-print(fooParte2Final)
+	fooParte1Final = ""
+	fooParte2Final = ""
 
-		
+	fooParte1Final = fooParte1[(cont):]
+	fooParte2Final = fooParte2[(cont):]
+	
+	StringFinal = StringFinal + fooParte1Final + fooParte2Final;
+	print(StringFinal)
 
-# StringFinal = StringFinal + (fooParte1Final + fooParte2Final);
-
-print(StringFinal)
+	contVezes += 1;
+print()
