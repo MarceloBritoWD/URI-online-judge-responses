dedc179a8a4c0ff9a1c15d626b66a07de9c705c4
Marcelo Brito <marcelobritowd@hotmail.com>
2016-09-08 23:46:16 -0300

    Questão Ad-hoc resolvida

---
+++ Ad-hoc/1387.py
@@ -0,0 +1,10 @@
+entrada = input().split()
+L = int(entrada[0])
+R = int(entrada[1])
+
+while L != 0 and R != 0:
+	print(L + R)
+
+	entrada = input().split()
+	L = int(entrada[0])
+	R = int(entrada[1])
