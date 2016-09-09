d33159d9708a435351a525cf7359224bc647e804
Marcelo Brito <marcelobritowd@hotmail.com>
2016-09-08 23:45:26 -0300

    Questão Ad-hoc resolvida

---
+++ Ad-hoc/1467.py
@@ -0,0 +1,17 @@
+while 1:
+	try:
+		entrada = input().split()
+		a = int(entrada[0])
+		b = int(entrada[1])
+		c = int(entrada[2])
+
+		if a != b and a != c:
+			print("A")
+		elif b != a and b != c:
+			print("B")
+		elif c != a and c != b:
+			print("C")
+		else:
+			print("*")
+	except:
+		break;
